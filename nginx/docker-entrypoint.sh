aabdfb7eac42afe35616853839c4207a7272cdf4
Maiko Bossuyt <hello@maiko-bossuyt.eu>
2020-05-07 04:33:36 +0200

    Update docker-entrypoint.sh

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -3,4 +3,4 @@ set -e
 
 sed -i "s/define('DOCKER', false)/define('DOCKER', true)/" /usr/share/nginx/sonerezh/app/Config/bootstrap.php
 
-exec "$@"
+/usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf

fb9647abb09552081a483ea072850d7054c7866e
Maiko Bossuyt <hello@maiko-bossuyt.eu>
2020-05-07 04:34:31 +0200

    Delete docker-entrypoint.sh

--- nginx/docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-sed -i "s/define('DOCKER', false)/define('DOCKER', true)/" /usr/share/nginx/sonerezh/app/Config/bootstrap.php
-
-/usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf

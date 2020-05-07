f81eed75538ced9bdc94a9e684ef3bf766ade243
Maiko Bossuyt <hello@maiko-bossuyt.eu>
2020-05-07 04:11:26 +0200

    Update docker-entrypoint.sh

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-sed -i "s/SONEREZH_DB_ENV_TYPE/${SONEREZH_DB_ENV_TYPE}/" /usr/share/nginx/sonerezh/app/Config/database.php
 sed -i "s/define('DOCKER', false)/define('DOCKER', true)/" /usr/share/nginx/sonerezh/app/Config/bootstrap.php
 
 exec "$@"
